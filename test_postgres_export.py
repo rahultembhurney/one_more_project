7fa15f1807ef9fc4cc4f7595936e3c2e0664dff1
Rahul <rtembhurney7@gmail.com>
2026-01-07 19:54:31 +0530

    removed test_load_dataset()

--- test_postgres_export.py
+++ test_postgres_export.py
@@ -5,15 +5,11 @@ import os
 with open("params.yaml", "r") as params:
     config = yaml.safe_load(params)
 
-def test_load_dataset(file_path:str):
-    df = pd.read_csv(file_path)
-    return df
-
-def test_check_empty_datastet(df:pd.DataFrame):
+def test_check_empty_datastet():
     file_path = os.path.join(config['postgres_conn']['feature_store_path'],
                              config['postgres_conn']['feature_store_folder_name'],  
                              config['postgres_conn']['feature_store_path'],)
-    df = test_load_dataset(file_path=file_path)
+    df = pd.read_csv(file_path)
     flag = df.shape[0]
     assert flag>0, "Dataset is empty"
 
