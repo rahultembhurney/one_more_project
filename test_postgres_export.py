bfea7f607ec384211ab2a32754ef3b38195a9cf6
Rahul <rtembhurney7@gmail.com>
2026-01-07 19:50:16 +0530

    renamed functions with prefix 'test' inside

--- test_postgres_export.py
+++ test_postgres_export.py
@@ -5,15 +5,15 @@ import os
 with open("params.yaml", "r") as params:
     config = yaml.safe_load(params)
 
-def load_dataset(file_path:str):
+def test_load_dataset(file_path:str):
     df = pd.read_csv(file_path)
     return df
 
-def check_empty_datastet(df:pd.DataFrame):
+def test_check_empty_datastet(df:pd.DataFrame):
     file_path = os.path.join(config['postgres_conn']['feature_store_path'],
                              config['postgres_conn']['feature_store_folder_name'],  
                              config['postgres_conn']['feature_store_path'],)
-    df = load_dataset(file_path=file_path)
+    df = test_load_dataset(file_path=file_path)
     flag = df.shape[0]
     assert flag>0, "Dataset is empty"
 
