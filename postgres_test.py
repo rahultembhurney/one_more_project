6d91a66ca3e0527c7b38e3b0eadd242f4d9be7bc
Rahul <rtembhurney7@gmail.com>
2026-01-07 19:35:52 +0530

    fixed pytest and ci.yaml

--- postgres_test.py
+++ postgres_test.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import yaml
+import os
+
+with open("params.yaml", "r") as params:
+    config = yaml.safe_load(params)
 
 def load_dataset(file_path:str):
     df = pd.read_csv(file_path)
     return df
 
 def check_empty_datastet(df:pd.DataFrame):
-    flag = df.value_counts().sum()
+    file_path = os.path.join(config['postgres_conn']['feature_store_path'],
+                             config['postgres_conn']['feature_store_folder_name'],  
+                             config['postgres_conn']['feature_store_path'],)
+    df = load_dataset(file_path=file_path)
+    flag = df.shape[0]
     assert(flag>0, "Dataset is empty")
+
